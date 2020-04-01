8f25d0076242a9bc29413979ee4dacd800714868
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2020-04-01 11:50:48 +0100

    rack-cors

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module Beapi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+      end
+    end
   end
 end
